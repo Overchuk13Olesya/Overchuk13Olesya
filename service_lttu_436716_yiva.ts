934f801efa7170e7c04f70ed77cc1486f2c83645
Overchuk13Olesya <olesyaoverchuk8@gmail.com>
2024-03-16 11:00:53 +0200

    Added UI layout

---
+++ service_lttu_436716_yiva.ts
@@ -0,0 +1,9 @@
+const removeDuplicates = array => Array.from(new Set(array));
+banana
+
+const sum = (a, b) => a + b;
+true - true
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+true + 0
+
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
